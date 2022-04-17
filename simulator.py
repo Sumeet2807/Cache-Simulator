7f2ad567d2ab0f9af7f469341c59b76ff4e6edda
Sumeet <sumeet2807a@gmail.com>
2022-04-17 02:42:30 -0400

    Added LFU and simulator modifications

--- simulator.py
+++ simulator.py
@@ -3,30 +3,51 @@ from cache import *
 from infra import *
 from events import *
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
-# initialize simulator environment
-# np.random.seed(11)
-files = Files()
-cache = LFU_Cache(files)
-# cache = Cache(files)
 
-sim = Simulator_Env(files,cache)
+cache_types = [Cache,LFU_Cache,LRU_Cache,MRU_Cache]
+for cache_type in cache_types:
+    total_times = []
+    queue_delays = []
+    req_served = []
+    for iteration in range(SIMULATOR_ITERATIONS):
 
-#initialize new req events to be processed at every second
-for i in range(TOTAL_TIME_TO_RUN):
-    E_get_new_reqs(sim,i)
+        # initialize simulator environment
+        # np.random.seed(11)
+        files = Files()
+        cache = cache_type(files)
+        # cache = Cache(files)
+
+        sim = Simulator_Env(files,cache)
+
+        #initialize new req events to be processed at every second
+        for i in range(TOTAL_TIME_TO_RUN):
+            E_get_new_reqs(sim,i)
 
 
-#Main simulator loop
-while(len(sim.sim_q.q)):
+        #Main simulator loop
+        while(len(sim.sim_q.q)):
 
-    e = sim.sim_q.pop()
-    e.process()
-    
+            e = sim.sim_q.pop()
+            e.process()    
 
-print(len(sim.log) == sim.req_count)
 
-print(sim.req_count)
-print(np.mean(sim.get_total_times_for_reqs()))
-print(np.mean(sim.queue_delays))
+        total_times.append(np.mean(sim.get_total_times_for_reqs()))
+        queue_delays.append(np.mean(sim.queue_delays))
+        req_served.append(sim.req_count)
+
+    print("Cache system - " + cache.__class__.__name__)
+    print('Average number of req served during simulations - ' + str(np.mean(req_served)))
+    print('Average Response rate for the requests - ' + str(np.mean(total_times)))
+    print('Average Queue delay in the process - ' + str(np.mean(queue_delays)))
+    p = sns.histplot(data=total_times,kde=True)
+    p.set_xlabel('time(sec)')
+    # p.set_xlim(0,10)
+
+    plt.show()
+    print('\n')
+    print('*********************XXXXXXXXXXXXXX*********************')
+    print('\n')
